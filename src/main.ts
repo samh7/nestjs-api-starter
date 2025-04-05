4c537c9c85dc6cf2dc8e8abf3347b23b5448918b
samh7 <samh74490@gmail.com>
2025-04-05 17:48:57 +0300

    Update frontend url to use .env variable

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './shared/filters/all-exceptions.filter';
+import { AppModule } from '#/app.module';
+import { AllExceptionsFilter } from '#/common/filters/all-exceptions.filter';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import environment from './common/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
   app.enableCors({
-    // connect to angular frontend
-    origin: 'http://localhost:4200',
+    origin: environment.FRONTEND_URL,
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  await app.listen(3000);
+  await app.listen(environment.PORT);
 }
 bootstrap();
